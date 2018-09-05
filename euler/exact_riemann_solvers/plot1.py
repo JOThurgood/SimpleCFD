b3dea14aa77d1cb20cf7c44abdc7dcc1fd2a5eb7
Jonathan Thurgood <tflv6@C17775472.local>
2018-09-05 22:11:21 +0100

    add test plots. move pstar to testing since old / not needed

--- euler/exact_riemann_solvers/plot1.py
+++ euler/exact_riemann_solvers/plot1.py
@@ -7,12 +7,18 @@ p = np.fromfile('p.dat', dtype=np.float64)
 u = np.fromfile('u.dat', dtype=np.float64)
 en = np.fromfile('en.dat', dtype=np.float64)
 
+#title = 'Test 1 - Sod at t=0.25'
+#title = 'Test 2 - 1-2-3 problem at t=0.15'
+#title = 'Test 3 - left half W+C blast at t=0.012'
+#title = 'Test 4 - right half W+C blast at t=0.035'
+#title = 'Test 5 - full W+C blast at t=0.035'
+title = 'your title here!'
 plt.plot(x, rho)
 if x.size < 129 :
   plt.plot(x,rho,'bx')
 plt.xlabel('x')
 plt.ylabel('rho')
-plt.title('title')
+plt.title(title)
 plt.grid(True)
 #plt.show()
 plt.savefig("rho.png")
@@ -23,7 +29,7 @@ if x.size < 129 :
   plt.plot(x,p,'bx')
 plt.xlabel('x')
 plt.ylabel('p')
-plt.title('title')
+plt.title(title)
 plt.grid(True)
 #plt.show()
 plt.savefig("p.png")
@@ -34,7 +40,7 @@ if x.size < 129 :
   plt.plot(x,u,'bx')
 plt.xlabel('x')
 plt.ylabel('u')
-plt.title('title')
+plt.title(title)
 plt.grid(True)
 #plt.show()
 plt.savefig("u.png")
@@ -45,7 +51,7 @@ if x.size < 129 :
   plt.plot(x,en,'bx')
 plt.xlabel('x')
 plt.ylabel('en')
-plt.title('title')
+plt.title(title)
 plt.grid(True)
 #plt.show()
 plt.savefig("en.png")

075500bcafc07731bf58665f9ec4f4a6c78e369e
Jonathan Thurgood <jonathanthurgood1989@gmail.com>
2018-10-16 12:02:18 +0100

    add explicit 1d diffusion solve

---
+++ diffusion/plot1.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.fromfile('x.dat', dtype=np.float64)
+phi = np.fromfile('phi.dat', dtype=np.float64)
+x_a = np.fromfile('x_analytic.dat', dtype=np.float64)
+phi_a = np.fromfile('phi_analytic.dat', dtype=np.float64)
+
+title = 'Explicit diffusion solve - red dash is analytic'
+#plt.plot(x, phi)
+#if x.size < 129 :
+plt.plot(x,phi,'bx')
+plt.plot(x_a,phi_a,'r--')
+phi = np.fromfile('phi.dat', dtype=np.float64)
+plt.xlabel('x')
+plt.ylabel('phi')
+plt.title(title)
+plt.grid(True)
+#plt.show()
+plt.savefig("x_phi.png")
+
+#\\\plt.clf()
+#\\\plt.plot(x, p)
+#\\\if x.size < 129 :
+#\\\  plt.plot(x,p,'bx')
+#\\\plt.xlabel('x')
+#\\\plt.ylabel('p')
+#\\\plt.title(title)
+#\\\plt.grid(True)
+#\\\#plt.show()
+#\\\plt.savefig("p.png")
+#\\\
+#\\\plt.clf()
+#\\\plt.plot(x, u)
+#\\\if x.size < 129 :
+#\\\  plt.plot(x,u,'bx')
+#\\\plt.xlabel('x')
+#\\\plt.ylabel('u')
+#\\\plt.title(title)
+#\\\plt.grid(True)
+#\\\#plt.show()
+#\\\plt.savefig("u.png")
+#\\\
+#\\\plt.clf()
+#\\\plt.plot(x, en)
+#\\\if x.size < 129 :
+#\\\  plt.plot(x,en,'bx')
+#\\\plt.xlabel('x')
+#\\\plt.ylabel('en')
+#\\\plt.title(title)
+#\\\plt.grid(True)
+#\\\#plt.show()
+#\\\plt.savefig("en.png")
+#\\\
+#\\\

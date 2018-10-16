b9a4595898464eb991a1bf3a44caa05e940cd9ad
Jonathan Thurgood <jonathanthurgood1989@gmail.com>
2018-10-16 12:16:17 +0100

    clean up comments and remove compiled binary from repository

--- diffusion/explicit1d.f90
+++ diffusion/explicit1d.f90
@@ -12,24 +12,25 @@ program explicit1d
   real(num), allocatable, dimension(:) :: phi, x, phi_new, phi_analytic
   integer :: out_unit = 10 
 
+  !user control
   t_end = 0.008_num
   nx = 16 
   x_min = 0.0_num
   x_max = 1.0_num
   x_c = (x_max - x_min) / 2.0_num
   nsteps = 100 !max number of steps
+  C = 0.8_num ! CFL-like factor in time step, le 1 required
 
+  !initial condition optin
 
-  C = 0.8_num ! CFL-like factor in time step
   k = 1.0 ! diffusion coefficient
+  t0 = 0.001_num !constant in initial condition, NOT the initial time
   time = 0.0_num !initial time
-
-  t0 = 0.001_num !constant in initial condition, stupid name
   phi2 = 2.0_num 
   phi1 = 1.0_num
 
 
-  ! initialise the arrays, 1 ghost cell needed
+  ! initialise the arrays and fill grid, 1 ghost cell needed
 
   allocate(x(0:nx+1))
   allocate(phi(0:nx+1))
@@ -39,9 +40,9 @@ program explicit1d
   x(0) = x_min - dx
   do ix = 1, nx+1
     x(ix) = x(ix-1) + dx
-!    print *,ix,x(ix)
   enddo
 
+  ! set the initial condition on phi
   phi = 0.0_num
   do ix = 1, nx 
     phi(ix) = (phi2-phi1)  * (t0/(time + t0))**0.5_num * &
@@ -56,46 +57,52 @@ program explicit1d
   dtk = dt * k 
   do
     if ((step >= nsteps .and. nsteps >= 0) .or. (time >= t_end)) exit
+
     step = step+1
     time = time + dt
     print *, step,time
 
-    !set bc 
-    phi(0) = 1.0_num
-    phi(nx+1) = 1.0_num
+    !set bc (simple Dirichlet)
+    phi(0) = phi1
+    phi(nx+1) = phi1
 
-    !loop in domain
+    !loop through domain
     do ix = 1, nx 
       phi_new(ix) = phi(ix) + & 
         & dtk * (phi(ix+1) - 2.0_num * phi(ix) + phi(ix-1))/dx**2 
-    enddo !ix = 1,nx
+    enddo !ix = 1, nx
 
     phi = phi_new
+
   enddo
 
-  ! produce a plot  at t_end
+  ! Output
 
   call execute_command_line("rm -rf x*.dat phi*.dat")
-     !^ dont stream to existing
+     !^ dont stream to existing *dat files
 
-
+  ! output numerical solution
   open(out_unit, file="x.dat", access="stream")
   write(out_unit) x(1:nx)
   close(out_unit)
-
   open(out_unit, file="phi.dat", access="stream")
   write(out_unit) phi(1:nx)
   close(out_unit)
 
-  !do the analytical solution with several times the resolution
-  deallocate(x)
-  nx = nx * 16 !this could get out of hand? probs ok in 1D
-  allocate(x(0:nx+1))
-  dx = (x_max - x_min) / REAL(nx-1,num) 
-  x(0) = x_min - dx
-  do ix = 1, nx+1
-    x(ix) = x(ix-1) + dx
-  enddo
+  ! calculate and output analytical sln and corresponding grid,
+  ! at several times the resolution to better-sample the continuous sln 
+
+  if (nx <= 2**14) then
+    deallocate(x)
+    nx = nx * 16 
+    allocate(x(0:nx+1))
+    dx = (x_max - x_min) / REAL(nx-1,num) 
+    x(0) = x_min - dx
+    do ix = 1, nx+1
+      x(ix) = x(ix-1) + dx
+    enddo
+  endif 
+
   allocate(phi_analytic(0:nx+1))
   do ix = 0,nx+1
     phi_analytic(ix) = (phi2-phi1)  * (t0/(time + t0))**0.5_num * &

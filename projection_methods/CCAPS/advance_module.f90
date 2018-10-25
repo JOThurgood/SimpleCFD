c46e74adcf0be1c48bc120b53bb5095b800355eb
Jonathan Thurgood <tflv6@C17775472.local>
2018-10-25 21:09:17 +0100

    add boundary conditions module

--- projection_methods/CCAPS/advance_module.f90
+++ projection_methods/CCAPS/advance_module.f90
@@ -1,6 +1,7 @@
 module advance_module
 
   use shared_data
+  use boundary_conditions
 
   implicit none
 
@@ -12,9 +13,12 @@ module advance_module
 
   subroutine advance
 
+    real(num) :: du, dv
+
     call set_dt 
     print *,'Step', step,'dt', dt
 
+    call velocity_bcs
     ! 1 - Calculate the advective velocities
 
     ! 1A -  calculate  time-centered interface states for the normal 
@@ -23,17 +27,17 @@ module advance_module
 
     do iy = -1, ny+2 ! limits for values on x boundaries, y center
     do ix = -2, nx+2 ! i.e. x faces
-      du = 
+      du = 0.0_num
         ! ^ slope of u at relative cc, (should be limited really)
       uhxl(ix,iy) = u(ix-1,iy) + & 
         & 0.5_num * (1.0_num - dt * u(ix-1,iy) / dx ) * du
-      du = 
+      du = 0.0_num
       uhxr(ix,iy) = u(ix,iy) - & 
         & 0.5_num * (1.0_num + dt * u(ix,iy) / dx) * du 
-      dv = 
+      dv = 0.0_num
       vhxl(ix,iy) = v(ix-1,iy) + & 
         & 0.5_num * (1.0_num - dt * u(ix-1,iy) / dx ) * dv
-      dv = 
+      dv = 0.0_num
       vhxr(ix,iy) = v(ix,iy) - & 
         & 0.5_num * (1.0_num + dt * u(ix,iy) / dx) * dv 
 
---
+++ projection_methods/CCAPS/boundary_conditions.f90
@@ -0,0 +1,18 @@
+module boundary_conditions
+
+  use shared_data
+
+  implicit none
+
+  private
+
+  public :: velocity_bcs
+
+  contains
+
+  subroutine velocity_bcs
+    print *,'velocity_bcs stub'
+  end subroutine velocity_bcs
+
+end module boundary_conditions
+

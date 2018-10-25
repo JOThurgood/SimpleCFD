4f7188097dfac8fa5708fdda340c48fbe5d98298
Jonathan Thurgood <tflv6@C17775472.local>
2018-10-25 21:04:46 +0100

    work on sec 1A

--- projection_methods/CCAPS/advance_module.f90
+++ projection_methods/CCAPS/advance_module.f90
@@ -23,7 +23,20 @@ module advance_module
 
     do iy = -1, ny+2 ! limits for values on x boundaries, y center
     do ix = -2, nx+2 ! i.e. x faces
-      uhxl(ix,iy) = 0.0
+      du = 
+        ! ^ slope of u at relative cc, (should be limited really)
+      uhxl(ix,iy) = u(ix-1,iy) + & 
+        & 0.5_num * (1.0_num - dt * u(ix-1,iy) / dx ) * du
+      du = 
+      uhxr(ix,iy) = u(ix,iy) - & 
+        & 0.5_num * (1.0_num + dt * u(ix,iy) / dx) * du 
+      dv = 
+      vhxl(ix,iy) = v(ix-1,iy) + & 
+        & 0.5_num * (1.0_num - dt * u(ix-1,iy) / dx ) * dv
+      dv = 
+      vhxr(ix,iy) = v(ix,iy) - & 
+        & 0.5_num * (1.0_num + dt * u(ix,iy) / dx) * dv 
+
     end do
     end do
 

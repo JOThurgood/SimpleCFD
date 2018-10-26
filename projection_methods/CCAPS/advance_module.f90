350995f01249d08d99f5181043dfb0deb038f9b6
Jonathan Thurgood <jonathanthurgood1989@gmail.com>
2018-10-26 14:33:32 +0100

    think step 1A is done

--- projection_methods/CCAPS/advance_module.f90
+++ projection_methods/CCAPS/advance_module.f90
@@ -25,24 +25,56 @@ module advance_module
     ! velocities
     ! (list var names here once you know exactly what is needed)
 
-    do iy =  1, ny ! limits for values on x boundaries, y center
-    do ix =  0, nx ! i.e. x faces
-      du = 0.0_num
-        ! ^ slope of u at relative cc, (should be limited really)
-      uhxl(ix,iy) = u(ix-1,iy) + & 
-        & 0.5_num * (1.0_num - dt * u(ix-1,iy) / dx ) * du
-      du = 0.0_num
-      uhxr(ix,iy) = u(ix,iy) - & 
-        & 0.5_num * (1.0_num + dt * u(ix,iy) / dx) * du 
-      dv = 0.0_num
-      vhxl(ix,iy) = v(ix-1,iy) + & 
-        & 0.5_num * (1.0_num - dt * u(ix-1,iy) / dx ) * dv
-      dv = 0.0_num
-      vhxr(ix,iy) = v(ix,iy) - & 
-        & 0.5_num * (1.0_num + dt * u(ix,iy) / dx) * dv 
+    ! be careful with indicies (conversion between i+1/2 type notation 
+    ! and separate indicies for xc and xb values)
 
-    end do
-    end do
+!    do iy =  1, ny ! limits for values on x boundaries, y center
+!    do ix =  0, nx ! i.e. x faces
+!
+!      du = minmod((u(ix-1,iy)-u(ix-2,iy))/dx, (u(ix,iy)-u(ix-1,iy))/dx)
+!
+!      uhxl(ix,iy) = u(ix-1,iy) + & 
+!        & 0.5_num * (1.0_num - dt * u(ix-1,iy) / dx ) * du
+!      du = 0.0_num
+!      uhxr(ix,iy) = u(ix,iy) - & 
+!        & 0.5_num * (1.0_num + dt * u(ix,iy) / dx) * du 
+!      dv = 0.0_num
+!      vhxl(ix,iy) = v(ix-1,iy) + & 
+!        & 0.5_num * (1.0_num - dt * u(ix-1,iy) / dx ) * dv
+!      dv = 0.0_num
+!      vhxr(ix,iy) = v(ix,iy) - & 
+!        & 0.5_num * (1.0_num + dt * u(ix,iy) / dx) * dv 
+!
+!    end do
+!    end do
+
+    ! maybe easier to iterate through the cc vars and assign to offset 
+
+    do iy = 1, ny
+    do ix = 1, nx  !xc counts from 1 to nx, <=0 and >nx are ghosts 
+  
+      du = minmod((u(ix,iy)-u(ix-1,iy))/dx,(u(ix+1,iy)-u(ix,iy)/dx))
+      uhxl(ix,iy) = u(ix,iy) + &
+        0.5_num * (1.0_num - dt * u(ix,iy) / dx ) * du
+
+      du = minmod((u(ix+1,iy)-u(ix,iy))/dx,(u(ix+2,iy)-u(ix+1,iy)/dx))
+      uhxr(ix,iy) = u(ix+1,iy) + &
+        0.5_num * (1.0_num - dt * u(ix+1,iy) / dx ) * du
+
+      dv = minmod((v(ix,iy)-v(ix-1,iy))/dx, (v(ix+1,iy)-v(ix,iy))/dx) 
+      vhxl(ix,iy) = v(ix,iy) + & 
+        & 0.5_num * (1.0_num - dt * u(ix,iy) / dx ) * dv
+      dv = minmod((v(ix+1,iy)-v(ix,iy))/dx, (v(ix+2,iy)-v(ix+1,iy))/dx) 
+      vhxr(ix,iy) = v(ix+1,iy) - & 
+        & 0.5_num * (1.0_num + dt * u(ix+1,iy) / dx) * dv 
+      
+
+
+    enddo 
+    enddo
+
+
+
 
   end subroutine advance
 
@@ -56,6 +88,16 @@ module advance_module
 
   end subroutine set_dt
 
+  real(num) function minmod(a,b)  
+    real(num), intent(in) :: a, b 
+    if ( (abs(a) < abs(b)) .and. (a*b > 0) ) then
+      minmod = a        
+    else if ( (abs(a) > abs(b)) .and. (a*b > 0) ) then
+      minmod = b        
+    else                
+      minmod = 0.0_num
+    endif               
+  end function minmod
 
 end module advance_module
 

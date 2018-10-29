95ded4f618e2faeca96051cd3545d3d59fbd2197
Jonathan Thurgood <jonathanthurgood1989@gmail.com>
2018-10-29 12:10:53 +0000

    add yface quantities and correct some sign and slope calc errors

--- projection_methods/CCAPS/advance_module.f90
+++ projection_methods/CCAPS/advance_module.f90
@@ -50,16 +50,18 @@ module advance_module
 
     ! maybe easier to iterate through the cc vars and assign to offset 
 
-    do iy = 0, ny 
+    ! x faced data 
+
+    do iy = 1, ny 
     do ix = 0, nx  !xb counts from 0 to nx, <0 and >nx are ghosts 
   
-      du = minmod((u(ix,iy)-u(ix-1,iy))/dx,(u(ix+1,iy)-u(ix,iy)/dx))
+      du = minmod((u(ix,iy)-u(ix-1,iy))/dx,(u(ix+1,iy)-u(ix,iy))/dx)
       uhxl(ix,iy) = u(ix,iy) + &
         0.5_num * (1.0_num - dt * u(ix,iy) / dx ) * du
 
-      du = minmod((u(ix+1,iy)-u(ix,iy))/dx,(u(ix+2,iy)-u(ix+1,iy)/dx))
-      uhxr(ix,iy) = u(ix+1,iy) + &
-        0.5_num * (1.0_num - dt * u(ix+1,iy) / dx ) * du
+      du = minmod((u(ix+1,iy)-u(ix,iy))/dx,(u(ix+2,iy)-u(ix+1,iy))/dx)
+      uhxr(ix,iy) = u(ix+1,iy) - &
+        0.5_num * (1.0_num + dt * u(ix+1,iy) / dx ) * du
 
       dv = minmod((v(ix,iy)-v(ix-1,iy))/dx, (v(ix+1,iy)-v(ix,iy))/dx) 
       vhxl(ix,iy) = v(ix,iy) + & 
@@ -72,9 +74,34 @@ module advance_module
     enddo 
     enddo
 
+    ! y faced data
+
+    do iy = 0, ny 
+    do ix = 1, nx
+
+      du = minmod( (u(ix,iy)-u(ix,iy-1))/dy, (u(ix,iy+1)-u(ix,iy))/dy)
+      uhyl(ix,iy) = u(ix,iy) + &
+        & 0.5_num * (1.0_num - dt * u(ix,iy) / dy) * du
+
+      du = minmod( (u(ix,iy+1)-u(ix,iy))/dy, (u(ix,iy+2)-u(ix,iy+1))/dy)
+      uhyr(ix,iy) = u(ix,iy+1) - &
+        & 0.5_num * (1.0_num + dt * u(ix,iy+1) / dy) * du
+
+      dv = minmod( (v(ix,iy)-v(ix,iy-1))/dy, (v(ix,iy+1)-v(ix,iy))/dy)
+      vhyl(ix,iy) = v(ix,iy) + &
+        & 0.5_num * (1.0_num - dt * v(ix,iy) / dy ) * dv
+
+      dv = minmod( (v(ix,iy+1)-v(ix,iy))/dy, (v(ix,iy+2)-v(ix,iy+1))/dy)
+      vhyr(ix,iy) = v(ix,iy+1) - &
+        & 0.5_num * (1.0_num + dt * v(ix,iy+1) / dy) * dv
+
+    enddo
+    enddo
+
+
+
     ! 1B - Use the normal velocities to calculate the advective vel
     ! by solving a Riemann problem
-
 
 
   

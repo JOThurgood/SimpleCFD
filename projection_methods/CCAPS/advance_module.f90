a02653ef9ee07231c3c303571719a44c4050b548
Jonathan Thurgood <jonathanthurgood1989@gmail.com>
2018-11-02 11:29:56 +0000

    tidy up step5

--- projection_methods/CCAPS/advance_module.f90
+++ projection_methods/CCAPS/advance_module.f90
@@ -425,30 +425,21 @@ module advance_module
 
     do iy = 1, ny
     do ix = 1, nx
-      !gpsi = (phi(ix+1,iy) - phi(ix,iy))/dx  
+
       gpsi = (phi(ix+1,iy) - phi(ix-1,iy))/dx/2.0_num
-
-      !gpsi = ( (phi(ix+1,iy)-phi(ix,iy))/dx + &
-      !  & (phi(ix,iy)-phi(ix-1,iy))/dx ) /2.0_num
-
-
       correction = dt * gpsi
       u(ix,iy) = ustar(ix,iy) - correction 
 
-      !gpsi = (phi(ix,iy+1)-phi(ix,iy))/dy
       gpsi = (phi(ix,iy+1)-phi(ix,iy-1))/dy/2.0_num
-
-      !gpsi = ( (phi(ix,iy+1)-phi(ix,iy))/dy + &
-      !  & (phi(ix,iy)-phi(ix,iy-1))/dy ) /2.0_num
-
-
       correction = dt * gpsi
       v(ix,iy) = vstar(ix,iy) - correction 
+
     enddo
     enddo
 
     ! calculate the divergence of the updated velocity field
     call velocity_bcs
+
     do iy = 1, ny
     do ix = 1, nx
       divu(ix,iy) = (u(ix+1,iy) - u(ix-1,iy))/dx/2.0_num &
@@ -464,11 +455,10 @@ module advance_module
   
     do ix = 1, nx
     do iy = 1, ny
-      !gpsi = (phi(ix+1,iy) - phi(ix,iy))/dx  
+
       gpsi = (phi(ix+1,iy) - phi(ix-1,iy))/dx/2.0_num
       gradp_x(ix,iy) = gradp_x(ix,iy) + gpsi
 
-      !gpsi = (phi(ix,iy+1)-phi(ix,iy))/dy
       gpsi = (phi(ix,iy+1)-phi(ix,iy-1))/dy/2.0_num
       gradp_y(ix,iy) = gradp_y(ix,iy) + gpsi
  

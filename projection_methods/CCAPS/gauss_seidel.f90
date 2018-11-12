7feae004a42d7dce30da216f0881958dfba6be0b
Jonathan Thurgood <jonathanthurgood1989@gmail.com>
2018-11-12 13:04:07 +0000

    introduce phigs which can be /= phi in main code

--- projection_methods/CCAPS/gauss_seidel.f90
+++ projection_methods/CCAPS/gauss_seidel.f90
@@ -12,8 +12,9 @@ module gauss_seidel
  
   contains
  
-  subroutine solve_gs(f,alpha,beta,use_old_phi,tol)
+  subroutine solve_gs(phigs,f,alpha,beta,use_old_phi,tol)
 
+    real(num), dimension(-1:nx+2,-1:ny+2), intent(inout) :: phigs
     real(num), dimension(1:nx,1:ny), intent(in) :: f
     real(num), intent(in) :: alpha, beta
     logical, intent(in) :: use_old_phi 
@@ -22,19 +23,20 @@ module gauss_seidel
     logical :: gsrb=.true. !hidden option
 
     real(num) :: L2, L2_old !norms
-    real(num) :: L_phi !laplacian of phi
-    integer :: maxir = 100000000
+    real(num) :: L_phigs !laplacian of phigs
+
+    integer :: maxir = -1 ! <0 for no max no of iterations
     integer :: ir = 0 
     logical :: verbose=.false. 
      
      
-    print *, '*** begining relaxation to solve for phi.'
+    print *, '*** begining GS relaxation solve.'
     print *, '*** this can take a while, use VERBOSE if you want to monitor stepping'
 
     if (.not. use_old_phi) then     
       phi = 0.0_num 
     else 
-      call phi_bcs !phi = 0.0_num
+      call phigs_bcs(phigs) !phi = 0.0_num
     endif 
 
     L2_old = 1e6_num
@@ -46,8 +48,8 @@ module gauss_seidel
       if (.not. gsrb) then 
         do iy = 1, ny  
         do ix = 1, nx  
-          phi(ix,iy) = phi(ix+1,iy) + phi(ix-1,iy) + phi(ix,iy+1) + phi(ix,iy-1)
-          phi(ix,iy) = (f(ix,iy) + beta * phi(ix,iy) / dx**2) / &
+          phigs(ix,iy) = phigs(ix+1,iy) + phigs(ix-1,iy) + phigs(ix,iy+1) + phigs(ix,iy-1)
+          phigs(ix,iy) = (f(ix,iy) + beta * phigs(ix,iy) / dx**2) / &
             & (alpha + 4.0_num * beta / dx**2)
 
 ! hardcoded poisson equation
@@ -61,8 +63,8 @@ module gauss_seidel
         do iy = 1, ny  
         do ix = 1, nx  
           if (modulo(ix+iy,2) == 1) then
-            phi(ix,iy) = phi(ix+1,iy) + phi(ix-1,iy) + phi(ix,iy+1) + phi(ix,iy-1)
-            phi(ix,iy) = (f(ix,iy) + beta * phi(ix,iy) / dx**2) / &
+            phigs(ix,iy) = phigs(ix+1,iy) + phigs(ix-1,iy) + phigs(ix,iy+1) + phigs(ix,iy-1)
+            phigs(ix,iy) = (f(ix,iy) + beta * phigs(ix,iy) / dx**2) / &
               & (alpha + 4.0_num * beta / dx**2)
 
 
@@ -77,8 +79,8 @@ module gauss_seidel
         do iy = 1, ny  
         do ix = 1, nx  
           if (modulo(ix+iy,2) == 0) then
-            phi(ix,iy) = phi(ix+1,iy) + phi(ix-1,iy) + phi(ix,iy+1) + phi(ix,iy-1)
-            phi(ix,iy) = (f(ix,iy) + beta * phi(ix,iy) / dx**2) / &
+            phigs(ix,iy) = phigs(ix+1,iy) + phigs(ix-1,iy) + phigs(ix,iy+1) + phigs(ix,iy-1)
+            phigs(ix,iy) = (f(ix,iy) + beta * phigs(ix,iy) / dx**2) / &
               & (alpha + 4.0_num * beta / dx**2)
 !            phi(ix,iy) = 0.25_num * ( & 
 !              & phi(ix+1,iy) + phi(ix-1,iy) + phi(ix,iy+1) + phi(ix,iy-1) &
@@ -91,17 +93,17 @@ module gauss_seidel
      
       ! Apply periodic boundary conditions on phi's ghost cells
      
-      call phi_bcs
+      call phigs_bcs(phigs)
      
       L2 = 0.0_num
       do iy = 1, ny  
       do ix = 1, nx  
-        L_phi = (phi(ix+1,iy) - 2.0_num*phi(ix,iy) + phi(ix-1,iy)) &
+        L_phigs = (phigs(ix+1,iy) - 2.0_num*phigs(ix,iy) + phigs(ix-1,iy)) &
           & / dx**2 + & 
-          & (phi(ix,iy+1) - 2.0_num*phi(ix,iy) + phi(ix,iy-1)) / dy**2 
+          & (phigs(ix,iy+1) - 2.0_num*phigs(ix,iy) + phigs(ix,iy-1)) / dy**2 
      
         !L2 = L2 + abs(divu(ix,iy)-L_phi)**2
-        L2 = L2 + abs(f(ix,iy)-(alpha*phi(ix,iy)-beta*L_phi))**2
+        L2 = L2 + abs(f(ix,iy)-(alpha*phigs(ix,iy)-beta*L_phigs))**2
       end do
       end do 
       L2 = sqrt( L2 / REAL(nx*ny,num))
@@ -126,5 +128,19 @@ module gauss_seidel
 
   end subroutine solve_gs 
 
+  subroutine phigs_bcs(phigs) 
+
+    real(num), dimension(-1:nx+2,-1:ny+2), intent(inout) :: phigs
+
+    phigs(0,:) = phigs(nx,:)
+    phigs(-1,:) = phigs(nx-1,:)
+    phigs(nx+1,:) = phigs(1,:)
+    phigs(nx+2,:) = phigs(2,:)
+    phigs(:,0) = phigs(:,ny)
+    phigs(:,-1) = phigs(:,ny-1)
+    phigs(:,ny+1) = phigs(:,1)
+    phigs(:,ny+2) = phigs(:,2)
+
+  end subroutine phigs_bcs
 
 end module gauss_seidel

faae80c4854f624a43c6dcd5854ef09debbf9b4c
Jonathan Thurgood <tflv6@C17775472.local>
2018-11-13 15:39:46 +0000

    add no slip for phigs

--- projection_methods/CCAPS/gauss_seidel.f90
+++ projection_methods/CCAPS/gauss_seidel.f90
@@ -167,6 +167,26 @@ module gauss_seidel
       phigs(:,ny+1) = phigs(:,ny)
       phigs(:,ny+2) = phigs(:,ny-1)
     endif
+
+    ! No slip
+
+    if (bc_xmin == no_slip) then
+      phigs(0,:) = -phigs(1,:)
+      phigs(-1,:) = -phigs(2,:)
+    endif
+    if (bc_xmax == no_slip) then
+      phigs(nx+1,:) = -phigs(nx,:)
+      phigs(nx+2,:) = -phigs(nx-1,:)
+    endif
+    if (bc_ymin == no_slip) then
+      phigs(:,0) = -phigs(:,1)
+      phigs(:,-1) = -phigs(:,2)
+    endif
+    if (bc_ymax == no_slip) then
+      phigs(:,ny+1) = -phigs(:,ny)
+      phigs(:,ny+2) = -phigs(:,ny-1)
+    endif
+
   end subroutine phigs_bcs
 
 end module gauss_seidel

d2f5b6b571d675502d8b9d5815316a3bfe09804d
JT <you@example.com>
2018-11-20 13:34:53 +0000

    attempt to help HSE during bootstrap

--- projection_methods/CCAPS/initial_conditions.f90
+++ projection_methods/CCAPS/initial_conditions.f90
@@ -139,7 +139,7 @@ module initial_conditions
     real(num) :: d, rho0, rho1
     real(num) :: fwtm , amp
 
-    amp = 0.00_num
+    amp = 0.01_num
 
     d = x_max - x_min
     fwtm = 0.01 * d
@@ -154,9 +154,20 @@ module initial_conditions
     do ix = -1, nx+1
     do iy = -1, ny+2
       x = xc(ix)
+
+      if (step /= 0) then ! trust post step0 to have HSE built in . dont call on bootstrap?
+          y = yc(iy) !+ amp * d * (cos(2.0_num * pi * x / d))
+          y = y * 1.818_num / fwtm
+          rho(ix,iy) = rho0 + 0.5_num * (rho1-rho0) * (1.0_num + tanh(y))
+          gradp_x(ix,iy) = rho(ix,iy) * grav_x
+          gradp_y(ix,iy) = rho(ix,iy) * grav_y
+      endif
+
+      !now overwrite rho with the perturbed + desingularised profile
       y = yc(iy) + amp * d * (cos(2.0_num * pi * x / d))
       y = y * 1.818_num / fwtm
       rho(ix,iy) = rho0 + 0.5_num * (rho1-rho0) * (1.0_num + tanh(y))
+
     enddo
     enddo
 

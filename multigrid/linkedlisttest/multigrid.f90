aacd59c409eafdec5556a89ec56d625bee59fc6d
JT <you@example.com>
2018-12-07 21:50:49 +0000

    basic grid heierarchy and initialisation routine complete

--- multigrid/linkedlisttest/multigrid.f90
+++ multigrid/linkedlisttest/multigrid.f90
@@ -14,8 +14,6 @@ module multigrid
     real(num),dimension(:,:), allocatable :: phi
     real(num),dimension(:,:), allocatable :: f
     type(grid), pointer :: next, prev
-!    contains
-!      procedure :: allocate_arrs => allocate_arrs
   end type grid
 
   type(grid), pointer :: head, tail
@@ -52,13 +50,24 @@ contains
     tail=>new_grid
   end subroutine add_grid
 
-!  subroutine create_grid(new_grid)
-!    type(grid), pointer :: new_grid
-!    allocate(new_grid)
-!    nullify(new_grid%next)
-!    nullify(new_grid%prev)
-!    new_grid%level = -1
-!  end subroutine create_grid
+  subroutine allocate_arrays(new_grid)
+    type(grid), pointer :: new_grid
+    allocate(new_grid%phi(0:new_grid%nx+1,0:new_grid%ny+1))
+    allocate(new_grid%f(1:new_grid%nx,1:new_grid%ny))
+  end subroutine allocate_arrays
+
+  subroutine grid_report(this)
+    type(grid), pointer :: this
+      print *,'******'
+      print *,'level',this%level
+      print *,'nx',this%nx
+      print *,'ny',this%ny
+      print *,'lbound phi',lbound(this%phi)
+      print *,'ubound phi',ubound(this%phi)
+      print *,'lbound f',lbound(this%f)
+      print *,'ubound f',ubound(this%f)
+      print *,'******'
+  end subroutine grid_report
 
 
   subroutine initialise_grids(nx,ny,nlevels)
@@ -76,11 +85,13 @@ contains
     nullify(head)
     nullify(tail)
 
+    ! create a linked list of grids with blank / unallocated data
     do lev = 1, nlevels
       call create_grid(new)
       call add_grid(new)
     enddo
 
+    ! go through the list and set up the arrays 
     current =>head
     lev = 0
     do while(associated(current))
@@ -88,14 +99,14 @@ contains
       current%level = lev 
       current%nx = nx / (2**(lev-1)) 
       current%ny = ny / (2**(lev-1)) 
+      call allocate_arrays(current)
       current=>current%next
     enddo
 
+    ! cycle through the list for a report to check all is set up good
     current => head
     do while(associated(current))
-      print *,'level',current%level
-      print *,'nx',current%nx
-      print *,'ny',current%ny
+      call grid_report(current)
       current=>current%next
     enddo 
  

8f45f8d7d7d0d5eec1c829a7da2fd01587fa3f0a
JT <you@example.com>
2018-12-15 15:20:46 +0000

    add plotting for test2

---
+++ multigrid/2D/test2_plots.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+nx = np.fromfile('test2_nx.dat', dtype = np.float64)
+L2 = np.fromfile('test2_l2.dat', dtype = np.float64)
+
+
+
+ax = plt.subplot(111)
+ax.set_xscale('log')
+ax.set_yscale('log')
+
+plt.scatter(nx, L2, marker="x")
+plt.plot(nx,L2[0]*(nx[0]/nx)**2,"--",color="r")
+
+plt.xlabel("n cells")
+plt.ylabel("test2: L2 norm - multigrid vs analytical")
+
+plt.savefig("test2_convergence.png")
